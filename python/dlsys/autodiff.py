26d51dc6d9ffefa833c3f53d28e9d55389ffc8e4
dheerajakula <dheerajakula0101@gmail.com>
2021-03-20 19:56:24 -0700

    modifying cuda file and autodiff.py

--- python/dlsys/autodiff.py
+++ python/dlsys/autodiff.py
@@ -153,6 +153,14 @@ class AddOp(Op):
     def infer_shape(self, node, input_shapes):
         """Need to handle input_vals[0].shape != input_vals[1].shape"""
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        if input_shapes[0] == input_shapes[1]:
+            return input_shapes[0]
+        if input_shapes[0] == (1,):
+            return input_shapes[1]
+        if input_shapes[1] == (1,):
+            return input_shapes[0]
+        return None
 
 
 class AddByConstOp(Op):
@@ -176,6 +184,8 @@ class AddByConstOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        return input_shapes[0]
 
 
 class MulOp(Op):
@@ -209,6 +219,14 @@ class MulOp(Op):
     def infer_shape(self, node, input_shapes):
         """Need to handle input_vals[0].shape != input_vals[1].shape"""
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        if input_shapes[0] == input_shapes[1]:
+            return input_shapes[0]
+        if input_shapes[0] == (1,):
+            return input_shapes[1]
+        if input_shapes[1] == (1,):
+            return input_shapes[0]
+        return None
 
 
 class MulByConstOp(Op):
@@ -232,6 +250,8 @@ class MulByConstOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        return input_shapes[0]
 
 
 class MatMulOp(Op):
@@ -300,6 +320,19 @@ class MatMulOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        if ((node.matmul_attr_trans_A is False) and
+                (node.matmul_attr_trans_B is False)):
+            return((input_shapes[0][0], input_shapes[1][1]))
+        elif ((node.matmul_attr_trans_A is True) and
+                (node.matmul_attr_trans_B is False)):
+            return((input_shapes[0][1], input_shapes[1][1]))
+        elif ((node.matmul_attr_trans_A is False) and
+                (node.matmul_attr_trans_B is True)):
+            return((input_shapes[0][0], input_shapes[1][0]))
+        elif ((node.matmul_attr_trans_A is True) and
+                (node.matmul_attr_trans_B is True)):
+            return((input_shapes[0][1], input_shapes[1][0]))
 
 
 class PlaceholderOp(Op):
@@ -339,6 +372,10 @@ class ZerosLikeOp(Op):
     def infer_shape(self, node, input_shapes):
         """If input_shape is a vector, simpler to return (1,)"""
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        if len(input_shapes[0]) == 1:
+            return (1,)
+        return input_shapes[0]
 
 
 class OnesLikeOp(Op):
@@ -362,6 +399,10 @@ class OnesLikeOp(Op):
     def infer_shape(self, node, input_shapes):
         """If input_shape is a vector, simpler to return (1,)"""
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        if len(input_shapes[0]) == 1:
+            return (1,)
+        return input_shapes[0]
 
 
 class ReduceSumAxisZeroOp(Op):
@@ -391,6 +432,10 @@ class ReduceSumAxisZeroOp(Op):
         for vector, simpler to do (3,)->(1,)
         """
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        if len(input_shapes[0]) == 1:
+            return (1,)
+        return input_shapes[0][1:]
 
 
 class BroadcastToOp(Op):
@@ -417,6 +462,8 @@ class BroadcastToOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        return broadcast_rule(input_shapes[0], input_shapes[1])
 
 
 def softmax_func(y):
@@ -453,6 +500,8 @@ class SoftmaxCrossEntropyOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        return (1,)
 
 
 class SoftmaxOp(Op):
@@ -476,6 +525,8 @@ class SoftmaxOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        return input_shapes[0]
 
 
 class ReluOp(Op):
@@ -497,6 +548,8 @@ class ReluOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 1
+        return input_shapes[0]
 
 
 class ReluGradientOp(Op):
@@ -520,6 +573,8 @@ class ReluGradientOp(Op):
 
     def infer_shape(self, node, input_shapes):
         """TODO: Your code here"""
+        assert len(input_shapes) == 2
+        return input_shapes[0]
 
 
 # Create global singletons of operators.
@@ -558,6 +613,7 @@ class Executor(object):
         self.node_to_shape_map = None
         self.node_to_arr_map = None
         self.feed_shapes = None
+        self.memory_pool = None
 
     def infer_shape(self, feed_shapes):
         """Given shapes of feed_dict nodes, infer shape for all nodes in graph.
@@ -571,6 +627,16 @@ class Executor(object):
         feed_shapes: node->shapes mapping for feed_dict nodes.
         """
         """TODO: Your code here"""
+        self.node_to_shape_map = {}
+        for node in feed_shapes:
+            self.node_to_shape_map[node] = feed_shapes[node]
+        for node in self.topo_order:
+            if node in self.node_to_shape_map:
+                continue
+            input_shapes = []
+            for inputs in node.inputs:
+                input_shapes.append(self.node_to_shape_map[inputs])
+            self.node_to_shape_map[node] = node.op.infer_shape(node, input_shapes)
 
     def memory_plan(self, feed_shapes):
         """Allocates ndarray.NDArray for every node except feed_dict nodes.
@@ -590,6 +656,24 @@ class Executor(object):
         feed_shapes: node->shapes mapping for feed_dict nodes.
         """
         """TODO: Your code here"""
+        if self.memory_pool == None:
+            self.memory_pool = []
+        if self.node_to_arr_map:
+            for node, arr in self.node_to_arr_map:
+                self.memory_pool.append(arr)
+        self.node_to_arr_map = {}
+        for node, shape in self.node_to_shape_map.items():
+            arr_node = None
+            if node in feed_shapes:
+                continue
+            for arr in self.memory_pool:
+                if arr.shape == shape:
+                    arr_node = arr
+                    self.memory_pool.remove(arr)
+                    break
+            if arr_node == None:
+                arr_node = ndarray.empty(shape, ctx=self.ctx)
+            self.node_to_arr_map[node] = arr_node
 
     def run(self, feed_dict, convert_to_numpy_ret_vals=False):
         """
--- src/gpu_op.cu
+++ src/gpu_op.cu
@@ -1,3 +1,4 @@
+
 #include "./c_runtime_api.h"
 #include <cassert>
 #include <cstdio>
@@ -14,81 +15,328 @@ __global__ void matrix_softmax_cross_entropy_kernel(int nrow, int ncol,
                                                     const float *input_b,
                                                     float *output) {
   // Dynamic shared memory, size provided at kernel launch.
-  extern __shared__ float loss_per_row[];
+    extern __shared__ float loss_per_row[];
   // Two dimensional thread blocks.
-  int y = blockIdx.x * blockDim.x * blockDim.y + threadIdx.y * blockDim.x +
-          threadIdx.x;
-  if (y >= nrow) {
-    return;
-  }
-  input_a += y * ncol;
-  input_b += y * ncol;
-  float maxval = *input_a;
-  // Find max for a row.
-  for (int x = 1; x < ncol; ++x) {
-    maxval = max(maxval, input_a[x]);
-  }
-  // Deduct by max for a row, and raise to exp.
-  float sum = 0;
-  for (int x = 0; x < ncol; ++x) {
-    sum += exp(input_a[x] - maxval);
-  }
-  // Compute per-row loss.
-  float loss = 0;
-  for (int x = 0; x < ncol; ++x) {
-    loss -= input_b[x] * log(exp(input_a[x] - maxval) / sum);
-  }
-  loss_per_row[y] = loss;
-  __syncthreads();
-  // Compute reduce_mean across rows.
-  float mean_loss = 0;
-  // Use a single thread to reduce mean across rows.
-  if ((threadIdx.x == 0) && (threadIdx.y == 0)) {
-    for (int i = 0; i < nrow; ++i) {
-      mean_loss += loss_per_row[i];
+    int y = blockIdx.x * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
+    if (y >= nrow) {
+        return;
     }
-    mean_loss /= nrow;
-    output[0] = mean_loss;
-  }
+    input_a += y * ncol;
+    input_b += y * ncol;
+    float maxval = *input_a;
+  // Find max for a row.
+    for (int x = 1; x < ncol; ++x) {
+        maxval = max(maxval, input_a[x]);
+    }
+  // Deduct by max for a row, and raise to exp.
+    float sum = 0;
+    for (int x = 0; x < ncol; ++x) {
+        sum += exp(input_a[x] - maxval);
+    }
+  // Compute per-row loss.
+    float loss = 0;
+    for (int x = 0; x < ncol; ++x) {
+        loss -= input_b[x] * log(exp(input_a[x] - maxval) / sum);
+    }
+    loss_per_row[y] = loss;
+    __syncthreads();
+  // Compute reduce_mean across rows.
+    float mean_loss = 0;
+  // Use a single thread to reduce mean across rows.
+    if ((threadIdx.x == 0) && (threadIdx.y == 0)) {
+        for (int i = 0; i < nrow; ++i) {
+            mean_loss += loss_per_row[i];
+        }
+        mean_loss /= nrow;
+        output[0] = mean_loss;
+    }
+}
+
+int DLGpuSoftmaxCrossEntropy(const DLArrayHandle input_a,
+                             const DLArrayHandle input_b,
+                             DLArrayHandle output) {
+    assert(input_a->ndim == 2);
+    assert(input_b->ndim == 2);
+    assert(output->ndim == 1);
+    assert(input_a->shape[0] == input_b->shape[0] && input_a->shape[1] == input_b->shape[1]);
+    int nrow = input_a->shape[0];
+  // Maximum x- or y-dimension of a block = 1024
+  // But we need 'nrow' shared memory, and max shared memory is 48KB.
+  // Conservatively allow max 16KB shared memory.
+    assert(nrow <= 1024 * 4);
+    int ncol = input_a->shape[1];
+    const float *input_data_a = (const float *)input_a->data;
+    const float *input_data_b = (const float *)input_b->data;
+    float *output_data = (float *)output->data;
+    dim3 threads;
+    if (nrow <= 1024) {
+        threads.x = nrow;
+    }
+    else {
+        threads.x = 1024;
+        threads.y = (nrow + 1023) / 1024;
+    }
+  // 1 block, each block with 'threads' number of threads with 'nrow' shared
+  // memory size
+    matrix_softmax_cross_entropy_kernel<<<1, threads, nrow * sizeof(float)>>>(
+        nrow, ncol, input_data_a, input_data_b, output_data);
+    return 0;
+}
+
+__global__ void array_set_kernel(int n, float *array_data, float value) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    array_data[y] = value;
 }
 
 int DLGpuArraySet(DLArrayHandle arr, float value) { /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < arr->ndim; i++) {
+        n *= arr->shape[i];
+    }
+    float *array_data = (float *)arr->data;
+    dim3 threads;
+    dim3 blocks;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    array_set_kernel<<<blocks, threads>>>(n, array_data, value);
+    return 0;
+}
+
+__global__ void broadcast_kernel(int n, int out, const float *input_data, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    for (int i = y; i < out;i += n) {
+        output_data[i] = input_data[y];
+    }
 }
 
 int DLGpuBroadcastTo(const DLArrayHandle input, DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    int out = 1;
+    for (int i = 0; i < output->ndim; i++) {
+        out *= output->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    broadcast_kernel<<<blocks, threads>>>(n, out, input_data, output_data);
+    return 0;
+}
+
+__global__ void reduce_sum_axis_zero_kernel(int n, int out, const float *input_data, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= out) {
+        return;
+    }
+    output_data[y] = 0;
+    for (int i = y; i < n; i += out) {
+        output_data[y] += input_data[i];
+    }
 }
 
 int DLGpuReduceSumAxisZero(const DLArrayHandle input, DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n =1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    int out = 1;
+    for (int i = 0; i < output->ndim; i++) {
+        out *= output->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    if (out <= 1024) {
+        threads.x = out;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (out + 1023) / 1024;
+    }
+    reduce_sum_axis_zero_kernel<<<blocks, threads>>>(n, out, input_data, output_data);
+    return 0;
+}
+
+__global__ void matrix_add_kernel(int n, const float *input_data_a, const float *input_data_b, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    output_data[y] = input_data_a[y] + input_data_b[y];
 }
 
 int DLGpuMatrixElementwiseAdd(const DLArrayHandle matA,
                               const DLArrayHandle matB, DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < matA->ndim; i++) {
+        n *= matA->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    const float *input_data_a = (const float *)matA->data;
+    const float *input_data_b = (const float *)matB->data;
+    float *output_data = (float *)output->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    matrix_add_kernel<<<blocks, threads>>>(n, input_data_a, input_data_b, output_data);
+    return 0;
+}
+
+__global__ void matrix_add_by_const_kernel(int n, const float *input_data, float val, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    output_data[y] = input_data[y] + val;
 }
 
 int DLGpuMatrixElementwiseAddByConst(const DLArrayHandle input, float val,
                                      DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    matrix_add_by_const_kernel<<<blocks, threads>>>(n, input_data, val, output_data);
+    return 0;
+}
+
+__global__ void matrix_ele_mul_kernel(int n, const float *input_data_a, const float *input_data_b, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    output_data[y] = input_data_a[y] * input_data_b[y];
 }
 
 int DLGpuMatrixElementwiseMultiply(const DLArrayHandle matA,
                                    const DLArrayHandle matB,
                                    DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < matA->ndim; i++) {
+        n *= matA->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data_a = (const float *)matA->data;
+    const float *input_data_b = (const float *)matB->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    matrix_ele_mul_kernel<<<blocks, threads>>>(n, input_data_a, input_data_b, output_data);
+    return 0;
+}
+
+__global__ void matrix_mul_by_const_kernel(int n, const float *input_data, float val, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    output_data[y] = input_data[y] * val;
 }
 
 int DLGpuMatrixMultiplyByConst(const DLArrayHandle input, float val,
                                DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    matrix_mul_by_const_kernel<<<blocks, threads>>>(n, input_data, val, output_data);
+    return 0;
+}
+
+__global__ void MatMulKernel(const float *A, const float *B, float *C, int rowA, int colA, int rowB, int colB, bool transA, bool transB) {
+    float Cvalue = 0.0;
+    int r = blockIdx.y * blockDim.y + threadIdx.y;
+    int c = blockIdx.x * blockDim.x + threadIdx.x;
+    if(!transA && !transB){
+        assert(colA == rowB);
+        if(r >= rowA || c >= colB) return;
+        for (int e = 0; e < colA; ++e)
+            Cvalue += (A[r * colA + e]) * (B[e * colB + c]);
+        C[r * colB + c] = Cvalue;
+    }else if(transA && !transB){
+        if(r >= colA || c >= colB) return;
+        for (int e = 0; e < rowA; ++e)
+            Cvalue += (A[e * colA + r]) * (B[e * colB + c]);
+        C[r * colB + c] = Cvalue;
+    }else if(!transA && transB){
+        if(r >= rowA || c >= rowB) return;
+        for (int e = 0; e < colA; ++e)
+            Cvalue += (A[r * colA + e]) * (B[c * colB + e]);
+        C[r * rowB + c] = Cvalue;
+    }else if(transA && transB){
+        if(r >= colA || c >= rowB) return;
+        for (int e = 0; e < rowA; ++e)
+            Cvalue += (A[e * colA + r]) * (B[c * colB + e]);
+        C[r * rowB + c] = Cvalue;
+    }
 }
 
 int DLGpuMatrixMultiply(const DLArrayHandle matA, bool transposeA,
@@ -97,52 +345,140 @@ int DLGpuMatrixMultiply(const DLArrayHandle matA, bool transposeA,
   /* TODO: Your code here */
   // Hint: use cublas
   // cublas assume matrix is column major
-  return 0;
+    int rowA = matA->shape[0];
+    int colA = matA->shape[1];
+    int rowB = matB->shape[0];
+    int colB = matB->shape[1];
+    float *matC_data = (float *)matC->data;
+    const float *matA_data = (const float *)matA->data;
+    const float *matB_data = (const float *)matB->data;
+    const int BLOCK_SIZE = 16;
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 dimGrid((max(rowB, colB) + dimBlock.x - 1) / dimBlock.x,
+            (max(rowA, colA) + dimBlock.y - 1) / dimBlock.y);
+    MatMulKernel<<<dimGrid, dimBlock>>>(matA_data, matB_data, matC_data, rowA, colA, rowB, colB, transposeA, transposeB);
+    return 0;
+}
+
+
+__global__ void relu_kernel(int n, const float *input_data, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    if (input_data[y] > 0.0) {
+        output_data[y] = input_data[y];
+    }
+    else {
+        output_data[y] = 0.0;
+    }
 }
 
 int DLGpuRelu(const DLArrayHandle input, DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    relu_kernel<<<blocks, threads>>>(n, input_data, output_data);
+    return 0;
+
+}
+
+__global__ void relu_gradient_kernel(int n, const float *input_data, const float *in_grad_data, float *output_data) {
+    int y = blockIdx.x * blockDim.x + threadIdx.x;
+    if (y >= n) {
+        return;
+    }
+    if (input_data[y] > 0.0) {
+        output_data[y] = in_grad_data[y];
+    }
+    else {
+        output_data[y] = 0.0;
+    }
 }
 
 int DLGpuReluGradient(const DLArrayHandle input, const DLArrayHandle in_grad,
                       DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
+    int n = 1;
+    for (int i = 0; i < input->ndim; i++) {
+        n *= input->shape[i];
+    }
+    dim3 blocks;
+    dim3 threads;
+    float *output_data = (float *)output->data;
+    const float *input_data = (const float *)input->data;
+    const float *in_grad_data = (const float *)in_grad->data;
+    if (n <= 1024) {
+        threads.x = n;
+        blocks.x = 1;
+    }
+    else {
+        threads.x = 1024;
+        blocks.x = (n + 1023) / 1024;
+    }
+    relu_gradient_kernel<<<blocks, threads>>>(n, input_data, in_grad_data, output_data);
+    return 0;
+
+}
+
+__global__ void matrix_softmax_kernel(int nrow, int ncol, const float *input_data, float *output_data) {
+    /*int y = blockIdx.x * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;*/
+    int y = threadIdx.y * blockDim.x + threadIdx.x;
+    if (y >= nrow) {
+        return;
+    }
+    input_data += y * ncol;
+    output_data += y * ncol;
+    float maxval = *input_data;
+    // Find max for a row.
+    for (int x = 1; x < ncol; ++x) {
+        maxval = max(maxval, input_data[x]);
+    }
+    // Deduct by max for a row, and raise to exp.
+    float sum = 0;
+    for (int x = 0; x < ncol; ++x) {
+        sum += exp(input_data[x] - maxval);
+    }
+    for (int x = 0; x < ncol; ++x) {
+        output_data[x] = exp(input_data[x] - maxval) / sum;
+    }
 }
 
 int DLGpuSoftmax(const DLArrayHandle input, DLArrayHandle output) {
   /* TODO: Your code here */
-  return 0;
-}
-
-int DLGpuSoftmaxCrossEntropy(const DLArrayHandle input_a,
-                             const DLArrayHandle input_b,
-                             DLArrayHandle output) {
-  assert(input_a->ndim == 2);
-  assert(input_b->ndim == 2);
-  assert(output->ndim == 1);
-  assert(input_a->shape[0] == input_b->shape[0] &&
-         input_a->shape[1] == input_b->shape[1]);
-  int nrow = input_a->shape[0];
+    assert(input->ndim == 2);
+    int nrow = input->shape[0];
   // Maximum x- or y-dimension of a block = 1024
   // But we need 'nrow' shared memory, and max shared memory is 48KB.
   // Conservatively allow max 16KB shared memory.
-  assert(nrow <= 1024 * 4);
-  int ncol = input_a->shape[1];
-  const float *input_data_a = (const float *)input_a->data;
-  const float *input_data_b = (const float *)input_b->data;
-  float *output_data = (float *)output->data;
-  dim3 threads;
-  if (nrow <= 1024) {
-    threads.x = nrow;
-  } else {
-    threads.x = 1024;
-    threads.y = (nrow + 1023) / 1024;
-  }
+    assert(nrow <= 1024 * 4);
+    int ncol = input->shape[1];
+    const float *input_data = (const float *)input->data;
+    float *output_data = (float *)output->data;
+    dim3 threads;
+    if (nrow <= 1024) {
+        threads.x = nrow;
+    }
+    else {
+        threads.x = 1024;
+        threads.y = (nrow + 1023) / 1024;
+    }
   // 1 block, each block with 'threads' number of threads with 'nrow' shared
   // memory size
-  matrix_softmax_cross_entropy_kernel<<<1, threads, nrow * sizeof(float)>>>(
-      nrow, ncol, input_data_a, input_data_b, output_data);
-  return 0;
+    matrix_softmax_kernel<<<1, threads>>>(nrow, ncol, input_data, output_data);
+    return 0;
 }
